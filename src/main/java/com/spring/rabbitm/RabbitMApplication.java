7875a78df94270e1d5f1d1670691e467ec9a03a6
GADO89 <gadomahmoud377@gmail.com>
2023-12-13 03:16:54 +0200

    create rabbit-mq project

---
+++ src/main/java/com/spring/rabbitm/RabbitMApplication.java
@@ -0,0 +1,13 @@
+package com.spring.rabbitm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RabbitMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/rabbitm/RabbitMApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rabbitm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
